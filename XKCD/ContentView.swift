f7e3b969970baa5900669124065ab199ed54fb3e
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-05-17 21:51:28 +0600

    Initial Commit

---
+++ XKCD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XKCD
+//
+//  Created by Sheikh Bayazid on 5/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XKCD/XKCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDApp.swift
+//  XKCD
+//
+//  Created by Sheikh Bayazid on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct XKCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
